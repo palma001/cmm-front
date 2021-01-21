d4e0800fd38653d9149da79c36ceabcb23a31d00
Luis Palma <palmaluis1997@gmail.com>
2021-01-21 08:47:42 -0400

    change

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="body">
-    <!-- <div class="row justify-center container-logo">
-      <div class="col-md-5 col-xl-4 col-lg-5 col-sm-10 col-xs-10 offset-sm-2 offset-lg-1 offset-xl-2 offset-md-1" style="position: relative;">
-      </div>
-    </div> -->
     <div class="row justify-center">
-      <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7 col-xs-11">
+      <div class="col-xl-3 col-lg-4 col-md-4 col-sm-7 col-xs-11">
         <q-card flat class="my-card" style="background-color: transparent !important;">
-          <q-card-section class="text-center">
+          <q-card-section class="text-center q-pt-none q-pb-none">
             <img src="../statics/logos/logo_dixcont.png" class="img">
           </q-card-section>
         </q-card>
