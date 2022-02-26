1829ff857d6e1a083dc21bad5fbfeb7f2948c149
Luis Palma <palmaluis1997@gmail.com>
2022-02-25 23:57:38 -0400

    login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,70 +1,62 @@
 <template>
-  <div class="row">
-    <div class="col-6" v-if="!$q.screen.lt.sm">
-      <img src="illustration.png" class="full-width flex flex-center" style="height: 100vh;">
-    </div>
-    <div :class="$q.screen.lt.sm ? 'col-12 flex flex-center' : 'col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6 flex flex-center q-mb-xl'">
-      <q-card flat class="my-card" style="width: 70%; background: transparent">
-        <q-card-section class="text-center">
-          <img src="logo.png" class="img">
-        </q-card-section>
-        <q-card-section class="bg-primary text-white">
-          <div class="text-h6">Iniciar sesión</div>
-        </q-card-section>
-        <q-card-section :class="!$q.dark.isActive ? 'bg-blue-grey-1' : 'bg-dark'">
-          <q-input
-            class="q-mt-sm"
-            color="primary"
-            v-model="username"
-            label="Correo"
-            ref="username"
-            name="username"
-            outlined
-            dense
-            @keyup.enter.native="login"
-            :rules="[val => !!val || 'El campo es requerido.']">
-            <template v-slot:prepend>
-              <q-icon name="email" />
-            </template>
-          </q-input>
-          <q-input
-            class="q-mt-md"
-            color="primary"
-            v-model="password"
-            label="Contraseña"
-            ref="password"
-            name="password"
-            type="password"
-            dense
-            outlined
-            @keyup.enter.native="login"
-            :rules="[val => !!val || 'El campo es requerido.']">
-            <template v-slot:prepend>
-              <q-icon name="lock"/>
-            </template>
-          </q-input>
-        </q-card-section>
-        <q-card-actions :class="!$q.dark.isActive ? 'bg-blue-grey-1' : 'bg-dark'">
-          <q-space/>
-          <q-btn
-            color="primary"
-            @click="login"
-            :disable="btnDisable">
-            <span v-if="btnDisable">
-              <q-spinner-hourglass
-                color="blue-1"
-                size="20px"
-                :disable="btnDisable"
-              />
-              Cargando...
-            </span>
-            <span v-if="!btnDisable">
-              Iniciar Sesión
-            </span>
-          </q-btn>
-        </q-card-actions>
-      </q-card>
-    </div>
+  <div class="flex flex-center q-mt-xl">
+    <q-card flat class="my-card" style="width: 40%">
+      <q-card-section class="bg-primary text-white">
+        <div class="text-h6">Iniciar sesión</div>
+      </q-card-section>
+      <q-card-section :class="!$q.dark.isActive ? 'bg-blue-grey-1' : 'bg-dark'">
+        <q-input
+          class="q-mt-sm"
+          color="primary"
+          v-model="username"
+          label="Correo"
+          ref="username"
+          name="username"
+          outlined
+          dense
+          @keyup.enter.native="login"
+          :rules="[val => !!val || 'El campo es requerido.']">
+          <template v-slot:prepend>
+            <q-icon name="email" />
+          </template>
+        </q-input>
+        <q-input
+          class="q-mt-md"
+          color="primary"
+          v-model="password"
+          label="Contraseña"
+          ref="password"
+          name="password"
+          type="password"
+          dense
+          outlined
+          @keyup.enter.native="login"
+          :rules="[val => !!val || 'El campo es requerido.']">
+          <template v-slot:prepend>
+            <q-icon name="lock"/>
+          </template>
+        </q-input>
+      </q-card-section>
+      <q-card-actions :class="!$q.dark.isActive ? 'bg-blue-grey-1' : 'bg-dark'">
+        <q-space/>
+        <q-btn
+          color="primary"
+          @click="login"
+          :disable="btnDisable">
+          <span v-if="btnDisable">
+            <q-spinner-hourglass
+              color="blue-1"
+              size="20px"
+              :disable="btnDisable"
+            />
+            Cargando...
+          </span>
+          <span v-if="!btnDisable">
+            Iniciar Sesión
+          </span>
+        </q-btn>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 <script>
