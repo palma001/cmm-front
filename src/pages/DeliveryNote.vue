9c1bcec4c2c8089fd224b84c86df08b10ee78f90
Luis Palma <palmaluis1997@gmail.com>
2022-05-05 18:42:44 -0400

    qr

--- src/pages/DeliveryNote.vue
+++ src/pages/DeliveryNote.vue
@@ -8,7 +8,9 @@
     <q-dialog v-model="openQr">
       <q-card class="my-card">
         <q-card-section class="q-pa-none">
-          <qrcode-stream @decode="onDecode"></qrcode-stream>
+          <qrcode-stream @decode="onDone"></qrcode-stream>
+          <!-- <qrcode-drop-zone @decode="onDone"></qrcode-drop-zone> -->
+          <qrcode-capture @decode="onDone"></qrcode-capture>
           <!-- <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
             <div class="absolute-bottom text-subtitle2 text-center">
               Escanear Guía
@@ -31,9 +33,28 @@ export default {
       openQr: false
     }
   },
+  created () {
+    console.log('ihola')
+    // window.QRScanner.scan(this.onDone)
+  },
   methods: {
-    onDecode (data) {
-      alert(data)
+    onDone (data) {
+      const dataEnter = data.split('\n')
+      const objectData = {}
+      dataEnter.map(data => {
+        const data2 = data.split(':')
+        if (data2.length === 2) {
+          const stringFormated = data2[0].replace(/"/g, '-').replaceAll('.', '_').replaceAll(' ', '')
+          objectData[stringFormated] = data2[1]
+        }
+      })
+      const placas = objectData.PLACAS.split('/')
+      objectData.CHUTO = placas[0]
+      objectData.BATEA = placas[1]
+      console.log(objectData)
+    },
+    error (error) {
+      alert(error)
     }
   }
 }
