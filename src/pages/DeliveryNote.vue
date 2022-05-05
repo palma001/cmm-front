6c11e991b1ca40d6e83d0640e16b86a22c9e3d67
Amable Sales <amablesales@gmail.com>
2022-05-05 18:10:49 -0400

    Formulario

--- src/pages/DeliveryNote.vue
+++ src/pages/DeliveryNote.vue
@@ -17,10 +17,95 @@
         </q-card-section>
       </q-card>
     </q-dialog>
+    <div class="q-gutter-sm">
+       <q-card class="my-card">
+        <q-card-section class="row justify-center q-col-gutter-sm">
+          <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+            <q-select rounded outlined v-model="model" :options="proveedor" label="Proveedor" />
+          </div>
+          <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+            <q-input rounded outlined v-model="text" label="Guía" disable/>
+          </div>
+          <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+            <q-input rounded outlined v-model="text" label="Fecha" disable />
+          </div>
+        </q-card-section>
+        </q-card>
+        <q-separator />
+        <q-card class="mycard">
+            <q-card-section>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-input rounded outlined v-model="text" label="Empresa" disable/>
+                </div>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-input rounded outlined v-model="text" label="RIF" disable />
+                </div>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-input rounded outlined v-model="text" label="Dirección de Origen" />
+                </div>
+            </q-card-section>
+        </q-card>
+        <q-card class="mycard">
+            <q-card-section>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-select rounded outlined v-model="model" :options="estado" label="Estado" />
+                </div>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-select rounded outlined v-model="model" :options="cliente" label="Cliente" />
+                </div>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-input rounded outlined v-model="text" label="RIF" disable />
+                </div>
+                <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                  <q-input rounded outlined v-model="text" label="Dirección de Destino" />
+                </div>
+            </q-card-section>
+        </q-card>
+        <q-card class="mycard">
+            <q-card-section>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                {{material}}
+              </div>
+            </q-card-section>
+        </q-card>
+        <q-card class="mycard">
+            <q-card-section>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Empresa de Transporte" disable />
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="RIF" disable/>
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Conductor" disable/>
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="CI" disable/>
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Marca del Vehículo" disable />
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Modelo" disable/>
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Placa del Chuto" disable/>
+              </div>
+              <div class="col-sm-12 col-md-4 col-md-2 col-xs-12">
+                <q-input rounded outlined v-model="text" label="Placa Batea" disable/>
+              </div>
+            </q-card-section>
+        </q-card>
+          <div class="q-gutter-sm" >
+             <q-btn unelevated rounded color="primary" label="Generar Nota de Entrega"/>
+             <q-btn unelevated rounded color="secondary" label="Limpiar" />
+           </div>
+        </div>
   </q-page>
 </template>
 
 <script>
+
 export default {
   data () {
     return {
@@ -28,7 +113,13 @@ export default {
        * Open dialog qr
        * @type {Boolean}
        */
-      openQr: false
+      openQr: false,
+      text: '',
+      proveedor: ['Coporación Macro Metal C.M.M. C.A.', 'Protocol Capital W.L.L, C.A.'],
+      cliente: ['Metalplast de Venezuela 2021 C.A.', 'Reciplast de Venezuela C.A.', 'Protocol Capital W.L.L, C.A.'],
+      estado: ['Anzoátegui', 'Carabobo', 'Zulia'],
+      model: '',
+      material: ''
     }
   },
   methods: {
@@ -42,5 +133,4 @@ export default {
 <style lang="sass" scoped>
 .my-card
   width: 100%
-  max-width: 250px
 </style>
