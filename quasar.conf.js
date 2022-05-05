7a8633a97145c5f2fee65c8c524d0236344fb986
Amable Sales <amablesales@gmail.com>
2022-05-05 15:37:25 -0400

    url

--- quasar.conf.js
+++ quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
-      host: '192.168.0.107',
+      host: '192.168.0.110',
       https: false,
       port: 8080,
       open: true // opens browser window automatically
