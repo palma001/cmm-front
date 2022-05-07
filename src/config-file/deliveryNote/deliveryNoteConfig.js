5006bab47e93d7bd3a93e4267e5177b0118da20e
Luis Palma <palmaluis1997@gmail.com>
2022-05-07 13:50:34 -0400

    change

---
+++ src/config-file/deliveryNote/deliveryNoteConfig.js
@@ -0,0 +1,686 @@
+import { date } from 'quasar'
+export const deliveryNoteConfig = [
+  {
+    classTag: 'infoUsers',
+    index: 0,
+    children: [
+      {
+        tabulated: {
+          name: 'serie_number',
+          align: 'left',
+          field: row => row.serie_number,
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'serie_number',
+          addible: false,
+          editable: false,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'guide_number',
+          align: 'left',
+          field: row => row.guide_number,
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'guide_number',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'material_supplier',
+          align: 'left',
+          field: row => `${row.material_supplier.document_number}-${row.material_supplier.name}`,
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'guide_number',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'client',
+          align: 'left',
+          field: row => `${row.client.document_number}-${row.client.name}`,
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'guide_number',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'driver_name',
+          align: 'left',
+          field: row => `${row.driver_document_number}-${row.driver_name}`,
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'driver_name',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'material',
+          align: 'left',
+          field: row => row.material,
+          sortable: true,
+          visible: true,
+          visibleColumn: false
+        },
+        actionable: {
+          propTag: 'material',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'start_date',
+          align: 'left',
+          field: row => date.formatDate(row.start_date, 'DD/MM/YYYY'),
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'start_date',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'deadline',
+          align: 'left',
+          field: row => date.formatDate(row.deadline, 'DD/MM/YYYY'),
+          sortable: true,
+          visible: true,
+          visibleColumn: true
+        },
+        actionable: {
+          propTag: 'deadline',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'created_at',
+          align: 'left',
+          field: row => date.formatDate(row.created_at, 'DD/MM/YYYY'),
+          sortable: true,
+          visible: true,
+          visibleColumn: false
+        },
+        actionable: {
+          propTag: 'created_at',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'origin_address',
+          align: 'left',
+          field: row => row.origin_address,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'origin_address',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'destination_address',
+          align: 'left',
+          field: row => row.destination_address,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'destination_address',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'vehicle_brand',
+          align: 'left',
+          field: row => row.vehicle_brand,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'vehicle_brand',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'vehicle_model',
+          align: 'left',
+          field: row => row.vehicle_model,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'vehicle_model',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'vehicle_plate',
+          align: 'left',
+          field: row => row.vehicle_plate,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'vehicle_plate',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'trailer_model',
+          align: 'left',
+          field: row => row.trailer_model,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'trailer_model',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'trailer_plate',
+          align: 'left',
+          field: row => row.trailer_plate,
+          sortable: true,
+          visible: true
+        },
+        actionable: {
+          propTag: 'trailer_plate',
+          addible: true,
+          editable: true,
+          type: 'String',
+          visibleLabel: true,
+          component: {
+            name: 'b-input',
+            props: {
+              type: 'text',
+              dense: true,
+              outlined: true
+            },
+            class: {
+              'col-xs-12': true,
+              'col-sm-12': true,
+              'col-md-12': true
+            },
+            directives: [
+              {
+                name: 'validate',
+                value: {
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        tabulated: {
+          name: 'action',
+          align: 'center'
+        }
+      }
+    ]
+  }
+]
+
+/**
+* Actions buttons
+* @type {Array} array buttons
+*/
+export const buttonsActions = [
+  {
+    color: 'secondary',
+    icon: 'print',
+    size: 'sm',
+    event: 'view-details'
+  }
+]
+
+export const propsPanelEdition = {
+  data: {},
+  buttons: [
+    {
+      name: 'cancel',
+      action: 'cancel',
+      label: false,
+      props: {
+        icon: 'highlight_off',
+        color: 'negative',
+        glossy: true,
+        size: '12px'
+      },
+      tooltip: {
+        text: 'cancel',
+        props: {
+          'content-class': 'bg-negative'
+        }
+      }
+    },
+    {
+      name: 'reset',
+      action: 'reset',
+      label: false,
+      props: {
+        icon: 'restore',
+        color: 'positive',
+        glossy: true,
+        size: '12px'
+      },
+      tooltip: {
+        text: 'reset',
+        props: {
+          'content-class': 'bg-positive'
+        }
+      }
+    },
+    {
+      name: 'update',
+      action: 'update',
+      label: false,
+      props: {
+        color: 'primary',
+        glossy: true,
+        size: '12px',
+        icon: 'edit'
+      },
+      tooltip: {
+        text: 'update',
+        props: {
+          'content-class': 'bg-primary'
+        }
+      }
+    }
+  ]
+}
